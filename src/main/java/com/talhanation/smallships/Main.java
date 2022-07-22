814e5e971fac7ba2d8ef84667eea68a15c0f6be0
Metroite <22453965+Metroite@users.noreply.github.com>
2022-07-22 20:40:10 +0200

    Revert "Revert "Merge branch '1.19-2.0.0-Alpha-0.3' into 1.18.2-2.0.0-Alpha-0.3""
    This reverts commit 8bd26024ea222ab29b36b0b402cd79685d10df76.

--- src/main/java/com/talhanation/smallships/Main.java
+++ src/main/java/com/talhanation/smallships/Main.java
@@ -14,7 +14,7 @@ import com.talhanation.smallships.inventory.BasicShipContainer;
 import com.talhanation.smallships.network.*;
 import de.maxhenkel.corelib.ClientRegistry;
 import de.maxhenkel.corelib.CommonRegistry;
-import net.minecraft.client.KeyMapping;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MenuType;
@@ -37,28 +37,21 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.network.IContainerFactory;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
-import org.lwjgl.glfw.GLFW;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import javax.annotation.Nullable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Mod("smallships")
 public class Main {
     public static final String MOD_ID = "smallships";
     public static SimpleChannel SIMPLE_CHANNEL;
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-    
-    public static KeyMapping SAIL_KEY;
-    public static KeyMapping SAIL_L_KEY;
-    public static KeyMapping SAIL_H_KEY;
-    public static KeyMapping INV_KEY;
-    public static KeyMapping CANNON_KEY;
-    public static KeyMapping FORWARD_KEY;
-    public static KeyMapping BACK_KEY;
-    public static KeyMapping LEFT_KEY;
-    public static KeyMapping RIGHT_KEY;
 
-    public static MenuType<BasicShipContainer> BASIC_SHIP_CONTAINER_TYPE;
+    private static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(ForgeRegistries.MENU_TYPES, Main.MOD_ID);
+    public static final RegistryObject<MenuType<BasicShipContainer>> BASIC_SHIP_CONTAINER_TYPE = MENU_TYPES.register("basic_ship_container_type", () -> IForgeMenuType.create(((windowId, inv, data) -> new BasicShipContainer(windowId, Objects.requireNonNull(getInvEntityByUUID(inv.player, data.readUUID())), inv))));
 
     public Main() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SmallShipsConfig.CONFIG);
@@ -91,19 +84,7 @@ public class Main {
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public void clientSetup(FMLClientSetupEvent event) {
-
-        FORWARD_KEY = ClientRegistry.registerKeyBinding("key.ship_forward", "category.smallships", GLFW.GLFW_KEY_W);
-        BACK_KEY = ClientRegistry.registerKeyBinding("key.ship_back", "category.smallships", GLFW.GLFW_KEY_S);
-        LEFT_KEY = ClientRegistry.registerKeyBinding("key.ship_left", "category.smallships", GLFW.GLFW_KEY_A);
-        RIGHT_KEY = ClientRegistry.registerKeyBinding("key.ship_right", "category.smallships", GLFW.GLFW_KEY_D);
-
-        SAIL_KEY = ClientRegistry.registerKeyBinding("key.ship_sail", "category.smallships", GLFW.GLFW_KEY_R);
-        INV_KEY = ClientRegistry.registerKeyBinding("key.ship_inventory", "category.smallships", GLFW.GLFW_KEY_I);
-        SAIL_L_KEY = ClientRegistry.registerKeyBinding("key.lower_ship_sail", "category.smallships", GLFW.GLFW_KEY_J);
-        SAIL_H_KEY = ClientRegistry.registerKeyBinding("key.higher_ship_sail", "category.smallships", GLFW.GLFW_KEY_K);
-        CANNON_KEY = ClientRegistry.registerKeyBinding("key.cannon_shoot", "category.smallships", GLFW.GLFW_KEY_SPACE);
-
-        ClientRegistry.registerScreen(Main.BASIC_SHIP_CONTAINER_TYPE, BasicShipInvScreen::new);
+        MenuScreens.register(Main.BASIC_SHIP_CONTAINER_TYPE.get(), BasicShipInvScreen::new);
 
         MinecraftForge.EVENT_BUS.register(new RenderEvents());
         MinecraftForge.EVENT_BUS.register(new PlayerEvents());
