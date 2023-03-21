e53ba2c7304fe19e4d44b798ef4796fe0ef4667a
Metroite <22453965+Metroite@users.noreply.github.com>
2023-03-21 04:59:08 +0100

    ability to zoom when in third person view

---
+++ common/src/main/java/com/talhanation/smallships/duck/CameraZoomAccess.java
@@ -0,0 +1,6 @@
+package com.talhanation.smallships.duck;
+
+public interface CameraZoomAccess {
+    double getShipZoomData();
+    void setShipZoomData(double zoom);
+}
---
+++ common/src/main/java/com/talhanation/smallships/mixin/client/CameraMixin.java
@@ -0,0 +1,36 @@
+package com.talhanation.smallships.mixin.client;
+
+import com.talhanation.smallships.duck.CameraZoomAccess;
+import com.talhanation.smallships.world.entity.ship.Ship;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(Camera.class)
+public abstract class CameraMixin implements CameraZoomAccess {
+    @Shadow public abstract Entity getEntity();
+
+    @ModifyConstant(method = "setup", constant = @Constant(doubleValue = 4.0))
+    private double setupShipZoom(double constant) {
+        if (this.getEntity().getVehicle() instanceof Ship && !Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
+            return this.getShipZoomData();
+        } else {
+            return constant;
+        }
+    }
+
+    private double shipZoom = 6.0D;
+
+    @Override
+    public double getShipZoomData() {
+        return this.shipZoom;
+    }
+
+    @Override
+    public void setShipZoomData(double d) {
+        this.shipZoom = d;
+    }
+}
