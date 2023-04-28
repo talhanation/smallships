1144cbb511ef189be37a604ea95f7560891134a4
talhanation <talhakantar@yahoo.com>
2023-04-28 15:41:34 +0200

    Update Paddleable.java

--- common/src/main/java/com/talhanation/smallships/world/entity/ship/abilities/Paddleable.java
+++ common/src/main/java/com/talhanation/smallships/world/entity/ship/abilities/Paddleable.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Mth;
 public interface Paddleable extends Ability {
     default void tickPaddleShip() {
         if (self().isVehicle()) {
-            self().setPaddleState(((BoatAccessor)self()).isInputRight() && !((BoatAccessor)self()).isInputLeft() || ((BoatAccessor)self()).isInputUp(), ((BoatAccessor)self()).isInputLeft() && !((BoatAccessor)self()).isInputRight() || ((BoatAccessor)self()).isInputUp());
+            self().setPaddleState(this.shouldPaddleLeft(), this.shouldPaddleRight());
         }
     }
 
@@ -17,15 +17,25 @@ public interface Paddleable extends Ability {
 
     default void animatePaddle(PaddleSide side, ModelPart modelPart, float f) {
         float f2 = self().getRowingTime(side.ordinal(), f);
+
         float xRotChange = Mth.clampedLerp(-1.0471976f, -0.2617994f, (Mth.sin(-f2) + 1.0f) / 2.0f);
         float yRotChange = Mth.clampedLerp(-0.7853982f, 0.7853982f, (Mth.sin(-f2 + 1.0f) + 1.0f) / 2.0f);
         if (side.equals(PaddleSide.LEFT)) {
             modelPart.yRot = -yRotChange;
             modelPart.xRot = 4.55F - (Mth.PI - xRotChange);
-        } else {
+        }
+        else {
             modelPart.yRot = Mth.PI + yRotChange;
             modelPart.xRot = (Mth.PI + 1.4F) + xRotChange;
         }
+    }
+
+    private boolean shouldPaddleLeft(){
+        return (((BoatAccessor)self()).isInputRight() && !((BoatAccessor)self()).isInputLeft()) || ((BoatAccessor)self()).isInputUp();
+    }
+
+    private boolean shouldPaddleRight(){
+        return (((BoatAccessor)self()).isInputLeft() && !((BoatAccessor)self()).isInputRight()) || ((BoatAccessor)self()).isInputUp();
     }
 
     enum PaddleSide {
