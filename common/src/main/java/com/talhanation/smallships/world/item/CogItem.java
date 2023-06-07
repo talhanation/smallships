7e1dac795836ac9fd2a72bb99c87f8a19683b2c9
Metroite <22453965+Metroite@users.noreply.github.com>
2023-06-07 18:39:24 +0200

    hopefully fix forge refmap warning + remove unnecessary override

--- common/src/main/java/com/talhanation/smallships/world/item/CogItem.java
+++ common/src/main/java/com/talhanation/smallships/world/item/CogItem.java
@@ -1,11 +1,7 @@
 package com.talhanation.smallships.world.item;
 
 import com.talhanation.smallships.world.entity.ship.CogEntity;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
@@ -13,11 +9,6 @@ import org.jetbrains.annotations.NotNull;
 public class CogItem extends ShipItem {
     public CogItem(Boat.Type type, Properties properties) {
         super(type, properties);
-    }
-
-    @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand interactionHand) {
-        return super.use(level, player, interactionHand);
     }
 
     @Override
