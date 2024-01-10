41036fcc0bcf775d44ac50b1b15cd919aef5e96f
talhanation <talhakantar@yahoo.com>
2024-01-10 08:53:45 +0100

    fixed collision damage if config is set to 0

--- common/src/main/java/com/talhanation/smallships/world/entity/ship/Ship.java
+++ common/src/main/java/com/talhanation/smallships/world/entity/ship/Ship.java
@@ -508,7 +508,7 @@ public abstract class Ship extends Boat {
     private void collisionDamage(Entity entity, float speed) {
         if (speed > 0.1F) {
             float damage = speed * SmallShipsConfig.Common.shipGeneralCollisionDamage.get().floatValue();
-            entity.hurt(ModDamageSourceTypes.shipCollision(this, this.getControllingPassenger()), damage);
+            if(damage > 0 && canDoCollisionDamage()) entity.hurt(ModDamageSourceTypes.shipCollision(this, this.getControllingPassenger()), damage);
         }
 
     }
