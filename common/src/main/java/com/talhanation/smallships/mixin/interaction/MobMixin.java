9017d3862b05cf1805c0cb7ba319667f0b6772a1
talhanation <talhakantar@yahoo.com>
2024-01-29 23:54:12 +0100

    fixed right click dismount not apply to Mobs

--- common/src/main/java/com/talhanation/smallships/mixin/interaction/MobMixin.java
+++ common/src/main/java/com/talhanation/smallships/mixin/interaction/MobMixin.java
@@ -2,7 +2,7 @@ package com.talhanation.smallships.mixin.interaction;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,20 +10,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Entity.class)
-public abstract class EntityMixin {
+@Mixin(Mob.class)
+public abstract class MobMixin {
     @SuppressWarnings("DataFlowIssue")
-    private Entity self() {
-        return (Entity)(Object)this;
+    private Mob self() {
+        return (Mob)(Object)this;
     }
 
-    @Inject(method = "interact", at =  @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "mobInteract", at =  @At(value = "HEAD"), cancellable = true)
     private void dismountEntityWhileInShip(Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> cir) {
         if (self().getVehicle() != null && self().getVehicle() instanceof Boat && !player.isCrouching()) {
             self().stopRiding();
             cir.setReturnValue(InteractionResult.SUCCESS);
         }
     }
-
 
 }
