13ab8ff1b7a88887c02828fbc5195a02038478df
Metroite <22453965+Metroite@users.noreply.github.com>
2024-08-10 05:42:56 +0200

    cleanup mixins

--- common/src/main/java/com/talhanation/smallships/mixin/container/MinecraftMixin.java
+++ common/src/main/java/com/talhanation/smallships/mixin/container/MinecraftMixin.java
@@ -16,44 +16,44 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class) // This Mixin prevents the mouse from repositioning in the middle of the screen. This is the default with Forge's NetworkHooks.openScreen and is therefore only needed for the Fabric version.
+@Mixin(Minecraft.class) // This Mixin prevents the mouse from repositioning in the middle of the screen. This is the default with Forge's NetworkHooks.openScreen and is therefore only needed for the Fabric and NeoForge versions.
 public abstract class MinecraftMixin {
     @Shadow @Final public MouseHandler mouseHandler;
     @Shadow @Nullable public Screen screen;
     @Shadow public abstract Window getWindow();
     @Unique
-    private Screen oldScreen;
+    private Screen smallships$oldScreen;
     @Unique
-    private int seenNull;
+    private int smallships$seenNull;
     @Unique
-    private double oldMousePosX;
+    private double smallships$oldMousePosX;
     @Unique
-    private double oldMousePosY;
+    private double smallships$oldMousePosY;
 
     @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "FIELD", target="Lnet/minecraft/client/Minecraft;screen:Lnet/minecraft/client/gui/screens/Screen;", opcode = Opcodes.PUTFIELD, shift = At.Shift.BEFORE))
     private void setScreenSaveMousePosition(Screen screen, CallbackInfo ci) {
         if (this.screen == null) { // There is always 2 null values in between each ShipContainerScreen while switching pages, this accounts for that
-            seenNull++;
-            if (seenNull > 2) this.oldScreen = null;
+            smallships$seenNull++;
+            if (smallships$seenNull > 2) this.smallships$oldScreen = null;
         } else {
-            seenNull = 0;
-            this.oldScreen = this.screen;
+            smallships$seenNull = 0;
+            this.smallships$oldScreen = this.screen;
         }
 
         if (this.screen instanceof ShipContainerScreen) {
-            this.oldMousePosX = this.mouseHandler.xpos();
-            this.oldMousePosY = this.mouseHandler.ypos();
+            this.smallships$oldMousePosX = this.mouseHandler.xpos();
+            this.smallships$oldMousePosY = this.mouseHandler.ypos();
         }
     }
 
     @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "INVOKE", target="Lnet/minecraft/client/gui/screens/Screen;init(Lnet/minecraft/client/Minecraft;II)V", shift = At.Shift.AFTER))
     private void setScreenApplyMousePosition(Screen screen, CallbackInfo ci) {
-        if (screen instanceof ShipContainerScreen && this.oldScreen instanceof ShipContainerScreen) {
+        if (screen instanceof ShipContainerScreen && this.smallships$oldScreen instanceof ShipContainerScreen) {
             // move and release mouse
-            ((MouseHandlerAccessor) this.mouseHandler).setXpos(this.oldMousePosX);
-            ((MouseHandlerAccessor) this.mouseHandler).setYpos(this.oldMousePosY);
+            ((MouseHandlerAccessor) this.mouseHandler).setXpos(this.smallships$oldMousePosX);
+            ((MouseHandlerAccessor) this.mouseHandler).setYpos(this.smallships$oldMousePosY);
             ((MouseHandlerAccessor) this.mouseHandler).setMouseGrabbed(false);
-            InputConstants.grabOrReleaseMouse(this.getWindow().getWindow(), 212993, this.oldMousePosX, this.oldMousePosY);
+            InputConstants.grabOrReleaseMouse(this.getWindow().getWindow(), 212993, this.smallships$oldMousePosX, this.smallships$oldMousePosY);
         }
     }
 }
