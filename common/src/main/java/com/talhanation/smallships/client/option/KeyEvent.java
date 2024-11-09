d5589c1d14556920decb0eb43bc403e3f1cfdcca
Chryfi <71967555+Chryfi@users.noreply.github.com>
2024-11-09 13:03:56 +0100

    feat: Add better KeyInputHandler

--- common/src/main/java/com/talhanation/smallships/client/option/KeyEvent.java
+++ common/src/main/java/com/talhanation/smallships/client/option/KeyEvent.java
@@ -1,6 +1,8 @@
 package com.talhanation.smallships.client.option;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.talhanation.smallships.network.ModPackets;
+import com.talhanation.smallships.network.packet.ServerboundEnterCannonBarrelPacket;
 import com.talhanation.smallships.network.packet.ServerboundShootGroundCannonPacket;
 import com.talhanation.smallships.network.packet.ServerboundShootShipCannonPacket;
 import com.talhanation.smallships.network.packet.ServerboundToggleShipSailPacket;
@@ -12,16 +14,29 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 
 public class KeyEvent {
-
     static boolean wasPressedSailKey = false;
+
+    public static void onKeyInput(int key, int scanCode, int action, int modifiers) {
+        final Player player;
+        if ((player = Minecraft.getInstance().player) == null) return;
+
+        if (player.getVehicle() instanceof GroundCannonEntity cannon) {
+            if (Minecraft.getInstance().options.keyJump.isDown()) {
+                cannon.trigger();
+            } else if (ModGameOptions.ENTER_CANNON_BARREL_KEY.matches(key, scanCode) && action == InputConstants.PRESS) {
+                cannon.putPlayerIntoBarrel(player);
+            }
+        }
+    }
+
     public static void onKeyInput(Minecraft client) {
         Player player = client.player;
         if (player == null) return;
         boolean pressedSailKey = ModGameOptions.SAIL_KEY.consumeClick();
+        boolean pressedEnterKey = ModGameOptions.ENTER_CANNON_BARREL_KEY.consumeClick();
         boolean pressedJumpKey = client.options.keyJump.isDown();
         if (player.getVehicle() instanceof Ship ship) {
             if (player.equals(ship.getDriver())) { // is driver
-
                 if(ship instanceof Sailable){
                     if (pressedSailKey && !wasPressedSailKey) {
                         ModPackets.clientSendPacket(new ServerboundToggleShipSailPacket());
@@ -38,10 +53,7 @@ public class KeyEvent {
                     if (!cannonable.self().isCannonKeyPressed())
                         ModPackets.clientSendPacket(new ServerboundShootShipCannonPacket(false));
                 }
-
             }
-        } else if (pressedJumpKey && player.getVehicle() instanceof GroundCannonEntity) {
-            ModPackets.clientSendPacket(new ServerboundShootGroundCannonPacket(false));
         }
     }
 }
--- common/src/main/java/com/talhanation/smallships/client/option/ModGameOptions.java
+++ common/src/main/java/com/talhanation/smallships/client/option/ModGameOptions.java
@@ -7,4 +7,5 @@ public class ModGameOptions {
     public static final String keyMappingCategory = "category.smallships";
 
     public static final KeyMapping SAIL_KEY = new KeyMapping("key.smallships.ship_sail", GLFW.GLFW_KEY_R, keyMappingCategory);
+    public static final KeyMapping ENTER_CANNON_BARREL_KEY = new KeyMapping("key.smallships.cannon_barrel_enter", GLFW.GLFW_KEY_F, keyMappingCategory);
 }
