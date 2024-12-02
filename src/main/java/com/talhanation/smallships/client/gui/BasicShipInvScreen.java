8d37df077a793ca4f6ab8307bfb1901a10e0041b
Saschap99 <78092049+RanPhx@users.noreply.github.com>
2024-12-02 14:57:42 +0100

    Reversal of changes
    Reversed  some changes which were not to be changed

--- src/main/java/com/talhanation/smallships/client/gui/BasicShipInvScreen.java
+++ src/main/java/com/talhanation/smallships/client/gui/BasicShipInvScreen.java
@@ -17,6 +17,7 @@ public class BasicShipInvScreen extends ScreenBase<BasicShipContainer> {
 
     private static final ResourceLocation GUI_TEXTURE = new ResourceLocation("textures/gui/container/generic_54.png");
 
+    private final BasicShipContainer container;
     private final AbstractInventoryEntity ship;
     private final Inventory playerInventory;
 
@@ -24,6 +25,7 @@ public class BasicShipInvScreen extends ScreenBase<BasicShipContainer> {
         super(GUI_TEXTURE, container, playerInventory, title);
         this.ship = container.getShip();
         this.playerInventory = playerInventory;
+        this.container = container;
         imageWidth = 176;
         imageHeight = 222;
     }
