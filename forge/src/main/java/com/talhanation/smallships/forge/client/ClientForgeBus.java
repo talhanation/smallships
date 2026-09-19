2fd2564aae5b039c94f6cd7c0874e69df6d383d8
talhanation <talhakantar@yahoo.com>
2026-09-19 13:30:16 +0200

    gui event forge cannon ammo handler

--- forge/src/main/java/com/talhanation/smallships/forge/client/ClientForgeBus.java
+++ forge/src/main/java/com/talhanation/smallships/forge/client/ClientForgeBus.java
@@ -1,9 +1,11 @@
 package com.talhanation.smallships.forge.client;
 
 import com.talhanation.smallships.SmallShipsMod;
+import com.talhanation.smallships.client.cannon.CannonAmmoHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,5 +23,10 @@ public class ClientForgeBus {
     static void onClientTick(TickEvent.ClientTickEvent event) {
         if (event.phase != TickEvent.Phase.END) return;
         com.talhanation.smallships.client.ClientTickHandler.onClientTick(Minecraft.getInstance());
+    }
+
+    @SubscribeEvent
+    static void onRenderGui(RenderGuiEvent.Post event) {
+        CannonAmmoHandler.render(event.getGuiGraphics());
     }
 }
